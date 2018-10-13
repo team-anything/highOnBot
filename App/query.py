ab1c49a8abcc9c79cb4aec10724ee3fd0e23100a
nurdtechie98 <nurdtechie98@gmail.com>
2018-10-14 02:51:48 +0530

    firebase :rocket:

---
+++ App/query.py
@@ -0,0 +1,32 @@
+import pyrebase
+
+config = {
+  "apiKey": "",
+  "authDomain": "",
+  "databaseURL": "",
+  "storageBucket": ""
+}
+
+firebase = pyrebase.initialize_app(config)
+
+auth = firebase.auth()
+
+# Log the user in
+user = auth.sign_in_with_email_and_password("","")
+
+# Get a reference to the database service
+db = firebase.database()
+
+def refresh(user):
+    user=auth.refresh(user['refreshToken'])
+
+def addUser(senderId,SSH,userid,password):
+  refresh(user)
+  users=db.get(user['idToken']).val()
+  users[senderId]=[SSH,userid,password]
+  db.update(users)
+
+def getUser(senderId):
+  refresh(user)
+  users=db.get(user['idToken']).val()
+  return users[senderId]
